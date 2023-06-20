b001bd4f02f03516de28230c1d02e5dab9c6abac
pedrovncs <pdrovncs@gmail.com>
2023-06-20 18:07:37 -0300

    corrigido alinhamento do marcador

--- src/components/shared/marcador/Marcador.jsx
+++ src/components/shared/marcador/Marcador.jsx
@@ -2,7 +2,7 @@ import styles from './Marcador.module.css';
 
 export default function Marcador({tipo, qtd}) {
   return(
-    <div className= {`marcador-${tipo} flex`}>
+    <div className={styles["marcador-flex"]}>
           <img src={`./src/components/shared/assets/${tipo}.svg`} alt={tipo} title={tipo} />
           <p>{qtd}</p>
     </div>
